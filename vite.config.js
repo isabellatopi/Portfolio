685b5cf09c52c992243c291d8862b028a16e034c
Samuel Solano <sdsolano@uninorte.edu.co>
2025-08-01 01:57:20 -0500

    hola

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // Nombre de tu repositorio
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: true, // Permite conexiones externas
     port: 5173, // Puerto por defecto de Vite
